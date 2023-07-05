42ec0af73cdb2a92e05d2d6c75b0aefc3b015993
unknown <kilcorse.michael92@gmail.com>
2023-07-05 17:00:27 -0400

    Fix server

---
+++ back/routes.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const Contact = require("./models/Contact")
+const router = express.Router()
+
+// Define a POST route for form submissions
+router.post("/contact", async (req, res) => {
+    const { email, subject, message } = req.body;
+    console.log("Server test")
+  
+    // Create a new Contact document using the form data
+    const contact = new Contact({ email, subject, message });
+  
+    // Save the document to the database
+    await contact
+      .save()
+      .then(() => {
+        res.status(200).json({ message: "Form submitted successfully" });
+        console.log("success")
+      })
+      .catch((error) => {
+        console.error("Error saving form data:", error);
+        res
+          .status(500)
+          .json({ message: "An error occurred while submitting the form" });
+      });
+  });
+
+  module.exports = router
--- back/server.js
+++ back/server.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require("./routes")
 const bodyParser = require("body-parser");
+const cors = require('cors')
 
 // Create Express app
 const app = express();
@@ -9,11 +11,12 @@ const port = 3000;
 // Configure body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors);
 
 // Connect to MongoDB/Atlas
 mongoose
   .connect(
-    "mongodb+srv://<username>:<password>@<cluster-url>/<database>?retryWrites=true&w=majority",
+    "mongodb+srv://dbUserPrime:0qgmwilMF9azhwCm@cluster0.vkkgk.mongodb.net/portfolioResponses?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -21,52 +24,20 @@ mongoose
   )
   .then(() => {
     console.log("Connected to MongoDB");
+    const app = express()
+		app.use(express.json()) // new
+		app.use("/api", routes)
+
+    // Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
 
-// Create a Mongoose schema for the form data
-const contactSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  subject: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-});
 
-// Create a Mongoose model based on the schema
-const Contact = mongoose.model("Contact", contactSchema);
 
-// Define a POST route for form submissions
-app.post("/api/contact", (req, res) => {
-  const { email, subject, message } = req.body;
 
-  // Create a new Contact document using the form data
-  const contact = new Contact({ email, subject, message });
 
-  // Save the document to the database
-  contact
-    .save()
-    .then(() => {
-      res.status(201).json({ message: "Form submitted successfully" });
-    })
-    .catch((error) => {
-      console.error("Error saving form data:", error);
-      res
-        .status(500)
-        .json({ message: "An error occurred while submitting the form" });
-    });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
